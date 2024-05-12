e5cff5f25f6ed7dfdc2cf78698ec605c19a80643
afanimartin <afani.dev@gmail.com>
2024-05-12 12:01:47 +0200

    Add model for school

--- db/migrate/20240512095532_create_schools.rb
+++ db/migrate/20240512095532_create_schools.rb
@@ -4,8 +4,8 @@ class CreateSchools < ActiveRecord::Migration[7.0]
       t.text :bio
       t.string :name
       t.string :phone
-      t.references :address, null: false, foreign_key: true
-      t.references :catgegory, null: false, foreign_key: true
+      t.references :address, type: :string, null: false, foreign_key: true
+      t.references :category, type: :string, null: false, foreign_key: true
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_12_094942) do
+ActiveRecord::Schema[7.0].define(version: 2024_05_12_095532) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,18 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_12_094942) do
     t.string "name", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "schools", force: :cascade do |t|
+    t.text "bio"
+    t.string "name"
+    t.string "phone"
+    t.string "address_id", null: false
+    t.string "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["address_id"], name: "index_schools_on_address_id"
+    t.index ["category_id"], name: "index_schools_on_category_id"
   end
 
   create_table "users", id: :string, force: :cascade do |t|
@@ -55,4 +67,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_12_094942) do
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
   end
 
+  add_foreign_key "schools", "addresses"
+  add_foreign_key "schools", "categories"
 end
